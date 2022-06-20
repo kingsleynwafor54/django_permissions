dfd5bc1b55d8f08c37e7cb4c7a9745e5ac27c38a
kingsleynwafor54 <61025076+kingsleynwafor54@users.noreply.github.com>
2022-06-20 21:33:37 +0100

    update

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -134,12 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-django_heroku.settings(locals())
